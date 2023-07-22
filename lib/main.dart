c038d4e9b2e78f502356504756966dfda18c681d
found-cake <skymin468@gmail.com>
2023-07-22 22:43:18 +0900

    upload files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
+import 'dart:io';
+
+import 'package:fastmedic/pages/response_page.dart';
 import 'package:fastmedic/pages/select_sick_page.dart';
 import 'package:fastmedic/providers/SelectSick.dart';
+import 'package:fastmedic/utils/toast.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var hasError = false;
+  await NaverMapSdk.instance.initialize(
+    clientId: "YOUR_ID",
+    onAuthFailed: (ex) {
+      sendToast("Error: ${ex.code}");
+      hasError = true;
+    }
+  );
+  if(hasError){
+    if(Platform.isIOS){
+      exit(0);
+    }else{
+      SystemNavigator.pop();
+    }
+  }else{
+    runApp(const MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget{
---
+++ lib/utils/toast.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+import 'package:fluttertoast/fluttertoast.dart';
+
+void sendToast(String message) => Fluttertoast.showToast(
+    msg: message,
+    gravity: ToastGravity.BOTTOM,
+    backgroundColor: const Color(0xff6E6E6E),
+    toastLength: Toast.LENGTH_LONG
+);
