fa3673b78460c8c3fb5c6762f9573cb1b9048ab8
found-cake <skymin468@gmail.com>
2023-07-22 16:29:41 +0900

    setup dependency for naver_map - TODO IOS setup

--- android/app/src/main/kotlin/io/github/f4s7m3d1c/fastmedic/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/f4s7m3d1c/fastmedic/MainActivity.kt
@@ -1,6 +1,11 @@
 package io.github.f4s7m3d1c.fastmedic
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
 }
