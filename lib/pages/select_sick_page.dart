2d721e83a13a04cdc55508b8be92094532135d73
Santpau <jsj140310@gmail.com>
2023-08-11 21:45:30 +0900

    test

--- lib/pages/select_sick_page.dart
+++ lib/pages/select_sick_page.dart
@@ -14,6 +14,7 @@ class SelectSickPage extends StatelessWidget{
           child: Column(
             children: [
               Text("main"),//TODO
+              Text('ㅁㄴㅇㄹ'),
             ],
           ),
         ),
