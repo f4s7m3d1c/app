7bedd9354838fde291d9b4109e59747ac1834ca2
Santpau <jsj140310@gmail.com>
2023-08-21 21:20:43 +0900

    Main_Page

--- lib/pages/select_sick_page.dart
+++ lib/pages/select_sick_page.dart
@@ -1,8 +1,9 @@
 import 'package:fastmedic/pages/basic_app_bar.dart';
 import 'package:fastmedic/pages/response_page.dart';
 import 'package:flutter/material.dart';
+import 'package:fastmedic/extensions/SymptomsContainer.dart';
 
-class SelectSickPage extends StatelessWidget{
+class SelectSickPage extends StatelessWidget {
   const SelectSickPage({super.key});
 
   @override
@@ -12,9 +13,60 @@ class SelectSickPage extends StatelessWidget{
         appBar: BasicAppBar,
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text("main"),//TODO
-              Text('ㅁㄴㅇㄹ'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Symptoms(
+                    ConditionName: '두통',
+                    images: 'assets/sick/Headache.jpg',
+                  ),
+                  Symptoms(
+                    ConditionName: '호흡',
+                    images: 'assets/sick/breath.jpg',
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Symptoms(
+                    ConditionName: '물집',
+                    images: 'assets/sick/blister.jpg',
+                  ),
+                  Symptoms(
+                    ConditionName: '충혈',
+                    images: 'assets/sick/congestion.jpg',
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Symptoms(
+                    ConditionName: '두통',
+                    images: 'assets/sick/Headache.jpg',
+                  ),
+                  Symptoms(
+                    ConditionName: '두통',
+                    images: 'assets/sick/Headache.jpg',
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Symptoms(
+                    ConditionName: '두통',
+                    images: 'assets/sick/Headache.jpg',
+                  ),
+                  Symptoms(
+                    ConditionName: '두통',
+                    images: 'assets/sick/Headache.jpg',
+                  ),
+                ],
+              ),
             ],
           ),
         ),
