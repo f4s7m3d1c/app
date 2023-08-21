3c1dce8e2b5d5256034389ac034b59d9004a4288
Santpau <jsj140310@gmail.com>
2023-08-21 22:30:32 +0900

    Main_Page-appbar add

--- lib/pages/select_sick_page.dart
+++ lib/pages/select_sick_page.dart
@@ -10,7 +10,44 @@ class SelectSickPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: BasicAppBar,
+        appBar: AppBar(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
+          ),
+          backgroundColor: Colors.white,
+          title:
+          Container( //검색창 UI
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset('assets/sick/fastmedic.jpg',
+                  width: 40, height: 40,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                const Row(
+                  children: [
+                    Text('Fast Medic',
+                      style: TextStyle(color: Colors.black,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      width: 170,
+                    ),
+                    Text('log',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
