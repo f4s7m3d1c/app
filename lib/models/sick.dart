387868bbd21f4393048a2a2513f917ccb337696f
found-cake <skymin468@gmail.com>
2023-09-16 00:02:20 +0900

    change name

--- lib/models/sick.dart
+++ lib/models/sick.dart
@@ -2,29 +2,29 @@
 
 import 'package:fastmedic/utils/assets.dart';
 
-enum Sick{
+enum Sick {
   cough, // 기침
   stomachache, // 복통
-  burns, // 두드러기
-  dyspnea, // 호흡곤란
+  burns, // 화상 나중에 두드러기
+  dyspnoea, // 호흡곤란
   headache, // 두통
   nausea, // 메스꺼움
   convulsion, // 경련
-  bulla, // 물집, 수포
-  hyperemia, // 충혈
+  blister, // 물집, 수포
+  congestion, // 충혈
   fever, // 발열
 }
 
-extension ParseToString on Sick{
+extension ParseToString on Sick {
   String toKeyword() {
-    switch(this) {
+    switch (this) {
       case Sick.cough:
         return "기침";
       case Sick.stomachache:
         return "복통";
       case Sick.burns:
-        return "두드러기";
-      case Sick.dyspnea:
+        return "화상";
+      case Sick.dyspnoea:
         return "호흡곤란";
       case Sick.headache:
         return "두통";
@@ -32,33 +32,34 @@ extension ParseToString on Sick{
         return "메스꺼움";
       case Sick.convulsion:
         return "경련";
-      case Sick.bulla:
+      case Sick.blister:
         return "수포";
-      case Sick.hyperemia:
+      case Sick.congestion:
         return "충혈";
       case Sick.fever:
         return "발열";
     }
   }
+
   String toImagePath() {
-    switch(this) {
+    switch (this) {
       case Sick.cough:
         return "${Assets.sick_images}cough.jpg";
       case Sick.stomachache:
         return "${Assets.sick_images}colic.jpg";
       case Sick.burns:
-        return "${Assets.sick_images}Burns.jpg";
-      case Sick.dyspnea:
+        return "${Assets.sick_images}burns.jpg";
+      case Sick.dyspnoea:
         return "${Assets.sick_images}breath.jpg";
       case Sick.headache:
-        return "${Assets.sick_images}Headache.jpg";
+        return "${Assets.sick_images}headache.jpg";
       case Sick.nausea:
-        return "${Assets.sick_images}convulsion.jpg";
+        return "${Assets.sick_images}sickness.jpg";
       case Sick.convulsion:
         return "${Assets.sick_images}convulsion.jpg";
-      case Sick.bulla:
+      case Sick.blister:
         return "${Assets.sick_images}blister.jpg";
-      case Sick.hyperemia:
+      case Sick.congestion:
         return "${Assets.sick_images}congestion.jpg";
       case Sick.fever:
         return "${Assets.sick_images}fever.jpg";
@@ -66,8 +67,8 @@ extension ParseToString on Sick{
   }
 
   String toKorean() {
-    switch(this) {
-      case Sick.bulla:
+    switch (this) {
+      case Sick.blister:
         return "물집, 수포";
       default:
         return toKeyword();
